bc5558d7e1f9f6ce7c7f107a15229225e76d8bb8
zhanghuazheng <18911341753@163.com>
2020-03-15 10:44:11 +0800

    商城项目初始化

---
+++ huazheng-mall/src/main/java/com/huazheng/mall/MallApplicationMain.java
@@ -0,0 +1,18 @@
+package com.huazheng.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanghuazheng
+ * @title: MallApplicationMain
+ * @projectName mall-pay
+ * @description: TODO
+ * @date 2020/3/15 10:40
+ */
+@SpringBootApplication
+public class MallApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplicationMain.class,args);
+    }
+}
