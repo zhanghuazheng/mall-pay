11b62adc30beb6951c01625225a3a722c1de8c62
zhanghuazheng <18911341753@163.com>
2020-03-15 18:33:21 +0800

    商城项目数据库层开发完成

---
+++ huazheng-mall/src/main/java/com/huazheng/mall/pojo/Order.java
@@ -0,0 +1,42 @@
+package com.huazheng.mall.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class Order implements Serializable {
+    private Integer id;
+
+    private Long orderNo;
+
+    private Integer userId;
+
+    private Integer shippingId;
+
+    private BigDecimal payment;
+
+    private Integer paymentType;
+
+    private Integer postage;
+
+    private Integer status;
+
+    private Date paymentTime;
+
+    private Date sendTime;
+
+    private Date endTime;
+
+    private Date closeTime;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+
+
+}
